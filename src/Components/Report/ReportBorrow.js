f428adc2bc7f5a079ec8de3e3ff2c9b0faff91ff
bank348 <chakkrapan348@gmail.com>
2020-12-20 23:13:40 +0700

    kuy

--- src/Components/Report/ReportBorrow.js
+++ src/Components/Report/ReportBorrow.js
@@ -1,17 +1,46 @@
-import React from 'react';
+
+import React,{useState} from "react" ;
 
 export default function Register (){
+
+  const [Date, setDate] = useState('');
+  const [Status, setStatus] = useState('');
+  const [Datelast, setDatelast] = useState('');
+
+  const onSubmit =(e) =>{
+    e.preventDefault();
+
+  }
+
+  const payload = {
+
+    Date,
+    Status,Datelast
+  }
+
+
+  console.log('submit payload',payload);
   return(
     
     <div className="HistoryCheck">
       <h2 class="Header">รายงานการยืมหนังสือ</h2> 
       <div class="Inputhis">
-      <form>
-        <label className="Date">วันที่</label>
-        <input type= "text" placeholder="วันที่ต้องการเช็ก" class="from-control" size="120" />  
+      <form  onSubmit = {onSubmit}>
+        <label className="ate">วันที่</label><br/>
+        <input type= "date" placeholder="วันที่ต้องการเช็ก" class="from-control" size="120" 
+        onChange = {(e) => setDate(e.target.value)}/> <br/><br/>
+        <label className="ate">ถึง</label><br/>
+        <input type= "date" placeholder="วันที่ต้องการเช็ก" class="from-control" size="120"
+         onChange = {(e) => setDatelast(e.target.value)}/><br/><br/>
+
+         <select class="select status" required aria-label="select example"  onChange = {(e) => setStatus(e.target.value)}>
+                        <option value="" >status</option>
+                        <option value="All" >All</option>
+                        <option value="Borrowing">Borrowing</option>
+                        <option value="Returned">Returned</option>
+                </select>  
         <div className="Checkbutton">
-            
-            <div className="conbutton"> 
+            <div className="cnbutton"> 
             <button class="btn btn-success" type="button"href="#" >ยืนยัน</button> </div> 
        </div>
      </form>
@@ -24,7 +53,5 @@ export default function Register (){
 
         );
  
-
-
+  }
   
-}
